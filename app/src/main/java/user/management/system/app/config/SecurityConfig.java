c83b0851de6cf9153a28f6d34ccf14f46bdf2bda
Bibek Aryal <bibekaryal86@gmail.com>
2024-06-25 09:03:56 -0600

    Flyway contd...

--- app/src/main/java/user/management/system/app/config/SecurityConfig.java
+++ app/src/main/java/user/management/system/app/config/SecurityConfig.java
@@ -2,18 +2,13 @@ package user.management.system.app.config;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-import static user.management.system.app.util.CommonUtils.getSystemEnvProperty;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import user.management.system.app.util.ConstantUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -42,15 +37,5 @@ public class SecurityConfig {
             // .and()
             .ignoring()
             .requestMatchers(GET, "/tests/ping");
-  }
-
-  @Bean
-  public InMemoryUserDetailsManager userDetailsService() {
-    UserDetails user =
-        User.withUsername(getSystemEnvProperty(ConstantUtils.BASIC_AUTH_USR, null))
-            .password("{noop}".concat(getSystemEnvProperty(ConstantUtils.BASIC_AUTH_PWD, null)))
-            .roles("USER")
-            .build();
-    return new InMemoryUserDetailsManager(user);
   }
 }
