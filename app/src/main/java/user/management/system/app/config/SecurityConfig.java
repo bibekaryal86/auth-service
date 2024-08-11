4cd1d4072faf97bcd09e79269f11e0faef78cfde
Bibek Aryal <bibekaryal86@gmail.com>
2024-08-11 15:08:37 -0600

    Security Config init

--- app/src/main/java/user/management/system/app/config/SecurityConfig.java
+++ app/src/main/java/user/management/system/app/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package user.management.system.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +14,16 @@ public class SecurityConfig {
   @Bean
   public BCryptPasswordEncoder bCryptPasswordEncoder() {
     return new BCryptPasswordEncoder(12);
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf(csrf -> csrf
+                    .ignoringRequestMatchers("/swagger-ui/**", "/v3/api-docs/**"))
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .anyRequest().authenticated());
+    return http.build();
   }
 }
