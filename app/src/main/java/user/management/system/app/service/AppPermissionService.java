bc22cf7b6b2671532bba7cbcb14b6942a07522b4
Bibek Aryal <bibekaryal86@gmail.com>
2024-08-13 12:06:04 -0600

    Remove Permissions Cache

--- app/src/main/java/user/management/system/app/service/AppPermissionService.java
+++ app/src/main/java/user/management/system/app/service/AppPermissionService.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import user.management.system.app.exception.ElementNotFoundException;
@@ -24,7 +22,6 @@ public class AppPermissionService {
   }
 
   // CREATE
-  @CacheEvict(value = "permissions", allEntries = true, beforeInvocation = true)
   public AppPermissionEntity createAppPermission(final AppPermissionRequest appPermissionRequest) {
     log.debug("Create App Permission: [{}]", appPermissionRequest);
     AppPermissionEntity appPermissionEntity = new AppPermissionEntity();
@@ -33,7 +30,6 @@ public class AppPermissionService {
   }
 
   // READ
-  @Cacheable(value = "permissions")
   public List<AppPermissionEntity> readAppPermissions() {
     log.debug("Read App Permissions...");
     return appPermissionRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
@@ -47,7 +43,6 @@ public class AppPermissionService {
   }
 
   // UPDATE
-  @CacheEvict(value = "permissions", allEntries = true, beforeInvocation = true)
   public AppPermissionEntity updateAppPermission(
       final int id, final AppPermissionRequest appPermissionRequest) {
     log.debug("Update App Permission: [{}], [{}]", id, appPermissionRequest);
@@ -57,7 +52,6 @@ public class AppPermissionService {
   }
 
   // DELETE
-  @CacheEvict(value = "permissions", allEntries = true, beforeInvocation = true)
   public AppPermissionEntity softDeleteAppPermission(final int id) {
     log.info("Soft Delete App Permission: [{}]", id);
     AppPermissionEntity appPermissionEntity = readAppPermission(id);
@@ -65,7 +59,6 @@ public class AppPermissionService {
     return appPermissionRepository.save(appPermissionEntity);
   }
 
-  @CacheEvict(value = "permissions", allEntries = true, beforeInvocation = true)
   public void hardDeleteAppPermission(final int id) {
     log.info("Hard Delete App Permission: [{}]", id);
     AppPermissionEntity appPermissionEntity = readAppPermission(id);
@@ -73,7 +66,6 @@ public class AppPermissionService {
   }
 
   // RESTORE
-  @CacheEvict(value = "permissions", allEntries = true, beforeInvocation = true)
   public AppPermissionEntity restoreSoftDeletedAppPermission(final int id) {
     log.info("Restore Soft Deleted App Permission: [{}]", id);
     AppPermissionEntity appPermissionEntity = readAppPermission(id);
