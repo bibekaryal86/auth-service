21e540c5119c37112fb5b1669611b68200ad5af5
Bibek Aryal <bibekaryal86@gmail.com>
2024-08-28 13:43:46 -0600

    Init

--- app/src/test/java/user/management/system/TestData.java
+++ app/src/test/java/user/management/system/TestData.java
@@ -1,10 +1,15 @@
 package user.management.system;
 
 import static user.management.system.app.util.ConstantUtils.ENV_KEY_NAMES;
+import static user.management.system.app.util.ConstantUtils.ENV_SERVER_PORT;
 
 public class TestData {
 
   public static void setSystemEnvPropertyTestData() {
-    ENV_KEY_NAMES.forEach(env -> System.setProperty(env, env));
+    ENV_KEY_NAMES.forEach(env -> {
+      if (!ENV_SERVER_PORT.equals(env)) {
+        System.setProperty(env, env);
+      }
+    });
   }
 }
