8d16c0764bdd089cc987a0615861765b479ce3c5
Bibek Aryal <bibekaryal86@gmail.com>
2024-09-04 21:46:51 -0600

    EntityDtoConvertUtilsTest--appUser

--- app/src/test/java/user/management/system/app/util/EntityDtoConvertUtilsTest.java
+++ app/src/test/java/user/management/system/app/util/EntityDtoConvertUtilsTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -14,6 +13,7 @@ import static org.mockito.Mockito.when;
 import helper.TestData;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +22,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import user.management.system.app.model.dto.AppRoleDto;
 import user.management.system.app.model.dto.AppUserAddressDto;
 import user.management.system.app.model.dto.AppUserDto;
 import user.management.system.app.model.dto.AppUserResponse;
@@ -65,7 +66,8 @@ public class EntityDtoConvertUtilsTest {
 
   @Test
   void testGetResponseSingleAppUser_NonNullEntity() {
-    when(appUserRoleService.readAppUserRoles(anyInt())).thenReturn(appUserRoleEntities);
+    when(appUserRoleService.readAppUserRoles(eq(1)))
+        .thenReturn(List.of(appUserRoleEntities.getFirst()));
     when(appRolePermissionService.readAppRolePermissions(any(), anyList()))
         .thenReturn(appRolePermissionEntities);
 
@@ -73,30 +75,48 @@ public class EntityDtoConvertUtilsTest {
     ResponseEntity<AppUserResponse> response =
         entityDtoConvertUtils.getResponseSingleAppUser(appUserEntity);
 
+    // check response entity
     assertNotNull(response);
     assertNotNull(response.getBody());
     assertNotNull(response.getBody().getUsers());
     assertEquals(1, response.getBody().getUsers().size());
     assertEquals(HttpStatus.OK, response.getStatusCode());
 
+    // check response dto against entity
     AppUserDto appUserDto = response.getBody().getUsers().getFirst();
     assertEquals(appUserDto.getId(), appUserEntity.getId());
     assertEquals(appUserDto.getEmail(), appUserEntity.getEmail());
     assertEquals(appUserDto.getCreatedDate(), appUserEntity.getCreatedDate());
 
+    // check addresses
     List<AppUserAddressEntity> appUserAddressEntities = appUserEntity.getAddresses();
     List<AppUserAddressDto> appUserAddressDtos = appUserDto.getAddresses();
 
     assertEquals(appUserAddressEntities.size(), appUserAddressDtos.size());
 
     for (int i = 0; i < appUserAddressEntities.size(); i++) {
-      AppUserAddressEntity appUserAddressEntity = appUserAddressEntities.get(i);
-      AppUserAddressDto appUserAddressDto = appUserAddressDtos.get(i);
-
-      assertEquals(appUserAddressEntity.getId(), appUserAddressDto.getId());
-      assertEquals(appUserAddressEntity.getAddressType(), appUserAddressDto.getAddressType());
-      assertEquals(appUserAddressEntity.getStreet(), appUserAddressDto.getStreet());
+      final int finalI = i + 1;
+      Optional<AppUserAddressEntity> appUserAddressEntity =
+          appUserAddressEntities.stream().filter(x -> x.getId() == finalI).findFirst();
+      Optional<AppUserAddressDto> appUserAddressDto =
+          appUserAddressDtos.stream().filter(y -> y.getId() == finalI).findFirst();
+      assertTrue(appUserAddressEntity.isPresent());
+      assertTrue(appUserAddressDto.isPresent());
+      assertEquals(appUserAddressEntity.get().getId(), appUserAddressDto.get().getId());
+      assertEquals(
+          appUserAddressEntity.get().getAddressType(), appUserAddressDto.get().getAddressType());
+      assertEquals(appUserAddressEntity.get().getStreet(), appUserAddressDto.get().getStreet());
     }
+
+    // check roles and permissions
+    List<AppRoleDto> appRoleDtos = appUserDto.getRoles();
+    assertEquals(1, appRoleDtos.size());
+    assertEquals(
+        appUserRoleEntities.getFirst().getAppRole().getId(), appRoleDtos.getFirst().getId());
+    assertEquals(1, appRoleDtos.getFirst().getPermissions().size());
+    assertEquals(
+        appRolePermissionEntities.getFirst().getAppPermission().getId(),
+        appRoleDtos.getFirst().getPermissions().getFirst().getId());
   }
 
   @Test
@@ -133,12 +153,17 @@ public class EntityDtoConvertUtilsTest {
 
     List<AppUserDto> appUserDtos = response.getBody().getUsers();
     for (int i = 0; i < appUserEntities.size(); i++) {
-      AppUserDto appUserDto = appUserDtos.get(i);
-      AppUserEntity appUserEntity = appUserEntities.get(i);
+      final int finalI = i + 1;
+      Optional<AppUserEntity> appUserEntity =
+          appUserEntities.stream().filter(x -> x.getId() == finalI).findFirst();
+      Optional<AppUserDto> appUserDto =
+          appUserDtos.stream().filter(y -> y.getId() == finalI).findFirst();
 
-      assertEquals(appUserDto.getId(), appUserEntity.getId());
-      assertEquals(appUserDto.getEmail(), appUserEntity.getEmail());
-      assertEquals(appUserDto.getCreatedDate(), appUserEntity.getCreatedDate());
+      assertTrue(appUserEntity.isPresent());
+      assertTrue(appUserDto.isPresent());
+      assertEquals(appUserEntity.get().getId(), appUserDto.get().getId());
+      assertEquals(appUserEntity.get().getEmail(), appUserDto.get().getEmail());
+      assertEquals(appUserEntity.get().getCreatedDate(), appUserDto.get().getCreatedDate());
     }
   }
 }
