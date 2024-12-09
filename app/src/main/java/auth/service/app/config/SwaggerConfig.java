9b55c0c5188197b774bab5e294d7820c8ca0a273
Bibek Aryal <bibekaryal86@gmail.com>
2024-12-08 22:24:50 -0700

    Mist

--- app/src/main/java/auth/service/app/config/SwaggerConfig.java
+++ app/src/main/java/auth/service/app/config/SwaggerConfig.java
@@ -1,24 +1,24 @@
 package auth.service.app.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.parser.OpenAPIV3Parser;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import lombok.extern.slf4j.Slf4j;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.StreamUtils;
 
-@Slf4j
 @Configuration
 public class SwaggerConfig {
 
   @Bean
-  public OpenAPI customOpenAPI() throws IOException {
-    ClassPathResource openApiResource = new ClassPathResource("openapi.json");
-    String openApiJson =
-        StreamUtils.copyToString(openApiResource.getInputStream(), StandardCharsets.UTF_8);
-    return new OpenAPIV3Parser().readContents(openApiJson, null, null).getOpenAPI();
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+            .info(
+                    new Info()
+                            .title("Auth Service")
+                            .description("Manage apps, users, roles and permissions")
+                            .contact(new Contact().name("Bibek Aryal"))
+                            .license(new License().name("Personal Use Only"))
+                            .version("1.0.1"));
   }
 }
