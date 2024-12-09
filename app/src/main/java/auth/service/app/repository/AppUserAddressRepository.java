0f9c907ef43064045b65f0b54360acbf023a0058
Bibek Aryal <bibekaryal86@gmail.com>
2024-12-09 16:02:15 -0700

    Deleted Repository

--- app/src/main/java/auth/service/app/repository/AppUserAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package auth.service.app.repository;
-
-import auth.service.app.model.entity.AppUserAddressEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppUserAddressRepository extends JpaRepository<AppUserAddressEntity, Integer> {}
--- app/src/main/java/auth/service/app/repository/AuditAppsRepository.java
+++
@@ -1,8 +0,0 @@
-package auth.service.app.repository;
-
-import auth.service.app.model.entity.AuditAppsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuditAppsRepository extends JpaRepository<AuditAppsEntity, Integer> {}
