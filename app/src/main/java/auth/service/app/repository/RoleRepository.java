0c411a11175056abe45da4ca355b7078d010922a
Bibek Aryal <bibekaryal86@gmail.com>
2024-12-10 22:02:45 -0700

    AuditService refactor

--- app/src/main/java/auth/service/app/repository/RoleRepository.java
+++ app/src/main/java/auth/service/app/repository/RoleRepository.java
@@ -4,10 +4,5 @@ import auth.service.app.model.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
-
 @Repository
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-    Optional<RoleEntity> findByRoleName(final String roleName);
-}
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {}
