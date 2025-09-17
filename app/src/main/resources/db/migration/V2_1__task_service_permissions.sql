f0b8cb4157980fc2224912110f5a88b7c4ba9153
Bibek Aryal <34973169+bibekaryal86@users.noreply.github.com>
2025-09-16 22:01:12 -0600

    Task Service Permissions and Sandbox (#92)

---
+++ app/src/main/resources/db/migration/V2_1__task_service_permissions.sql
@@ -0,0 +1,124 @@
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_TASK_CREATE', 'Can Add Task');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_TASK_READ', 'Can View Task(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_TASK_UPDATE', 'Can Update Task');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_TASK_DELETE', 'Can Delete Task');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_TASK_CREATE', 'Can Add Task');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_TASK_READ', 'Can View Task(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_TASK_UPDATE', 'Can Update Task');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_TASK_DELETE', 'Can Delete Task');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_TASK_CREATE', 'Can Add Task');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_TASK_READ', 'Can View Task(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_TASK_UPDATE', 'Can Update Task');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (4, 'TASKSVC_TASK_READ', 'Can View Task(s)');
+
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_TASK_INSTANCE_CREATE', 'Can Add Task Instance');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_TASK_INSTANCE_UPDATE', 'Can Update Task Instance');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_TASK_INSTANCE_DELETE', 'Can Delete Task Instance');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_TASK_INSTANCE_CREATE', 'Can Add Task Instance');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_TASK_INSTANCE_UPDATE', 'Can Update Task Instance');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_TASK_INSTANCE_DELETE', 'Can Delete Task Instance');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_TASK_INSTANCE_CREATE', 'Can Add Task Instance');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_TASK_INSTANCE_UPDATE', 'Can Update Task Instance');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (4, 'TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
+
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_RECURRENCE_PATTERN_CREATE', 'Can Add Recurrence Pattern');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_RECURRENCE_PATTERN_READ', 'Can View Recurrence Pattern(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_RECURRENCE_PATTERN_UPDATE', 'Can Update Recurrence Pattern');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_RECURRENCE_PATTERN_DELETE', 'Can Delete Recurrence Pattern');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_RECURRENCE_PATTERN_CREATE', 'Can Add Recurrence Pattern');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_RECURRENCE_PATTERN_READ', 'Can View Recurrence Pattern(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_RECURRENCE_PATTERN_UPDATE', 'Can Update Recurrence Pattern');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_RECURRENCE_PATTERN_DELETE', 'Can Delete Recurrence Pattern');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_RECURRENCE_PATTERN_CREATE', 'Can Add Recurrence Pattern');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_RECURRENCE_PATTERN_READ', 'Can View Recurrence Pattern(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_RECURRENCE_PATTERN_UPDATE', 'Can Update Recurrence Pattern');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (4, 'TASKSVC_RECURRENCE_PATTERN_READ', 'Can View Recurrence Pattern(s)');
+
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_NOTIFICATION_SETTING_CREATE', 'Can Add Notification Setting');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_NOTIFICATION_SETTING_READ', 'Can View Notification Setting(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_NOTIFICATION_SETTING_UPDATE', 'Can Update Notification Setting');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_NOTIFICATION_SETTING_DELETE', 'Can Delete Notification Setting');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_NOTIFICATION_SETTING_CREATE', 'Can Add Notification Setting');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_NOTIFICATION_SETTING_READ', 'Can View Notification Setting(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_NOTIFICATION_SETTING_UPDATE', 'Can Update Notification Setting');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_NOTIFICATION_SETTING_DELETE', 'Can Delete Notification Setting');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_NOTIFICATION_SETTING_CREATE', 'Can Add Notification Setting');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_NOTIFICATION_SETTING_READ', 'Can View Notification Setting(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_NOTIFICATION_SETTING_UPDATE', 'Can Update Notification Setting');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (4, 'TASKSVC_NOTIFICATION_SETTING_READ', 'Can View Notification Setting(s)');
+
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_NOTIFICATION_CREATE', 'Can Add Notification');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_NOTIFICATION_UPDATE', 'Can Update Notification');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (1, 'TASKSVC_NOTIFICATION_DELETE', 'Can Delete Notification');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_NOTIFICATION_CREATE', 'Can Add Notification');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_NOTIFICATION_UPDATE', 'Can Update Notification');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (2, 'TASKSVC_NOTIFICATION_DELETE', 'Can Delete Notification');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_NOTIFICATION_CREATE', 'Can Add Notification');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (3, 'TASKSVC_NOTIFICATION_UPDATE', 'Can Update Notification');
+INSERT INTO permission (role_id, permission_name, permission_desc)
+VALUES (4, 'TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
