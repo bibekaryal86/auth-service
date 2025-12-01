fa3c47149e7382c2a51750b55f26c18fc23ff786
Bibek Aryal <bibekaryal86@gmail.com>
2025-11-30 20:37:42 -0700

    Cleanup #2

---
+++ app/src/main/java/auth/service/app/repository/PlatformRolePermissionRepository.java
@@ -0,0 +1,12 @@
+package auth.service.app.repository;
+
+import auth.service.app.model.entity.PlatformRolePermissionEntity;
+import auth.service.app.model.entity.PlatformRolePermissionId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlatformRolePermissionRepository
+    extends JpaRepository<PlatformRolePermissionEntity, PlatformRolePermissionId> {
+
+}
