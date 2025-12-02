937e59f5091adc7fe4ab77af0122aa7039041412
Bibek Aryal <bibekaryal86@gmail.com>
2025-12-01 18:41:48 -0700

    PlatformService

--- app/src/main/java/auth/service/app/service/PlatformService.java
+++ app/src/main/java/auth/service/app/service/PlatformService.java
@@ -1,10 +1,93 @@
 package auth.service.app.service;
 
+import auth.service.app.exception.ElementNotActiveException;
+import auth.service.app.exception.ElementNotFoundException;
+import auth.service.app.model.dto.PlatformRequest;
+import auth.service.app.model.entity.PlatformEntity;
+import auth.service.app.repository.PlatformRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class PlatformService {}
+public class PlatformService {
+
+  private final PlatformRepository platformRepository;
+  private final PlatformProfileRoleService platformProfileRoleService;
+  private final PlatformRolePermissionService platformRolePermissionService;
+
+  // CREATE
+  public PlatformEntity createPlatform(final PlatformRequest platformRequest) {
+    log.debug("Create Platform: [{}]", platformRequest);
+    PlatformEntity platformEntity = new PlatformEntity();
+    BeanUtils.copyProperties(platformRequest, platformEntity);
+    return platformRepository.save(platformEntity);
+  }
+
+  // READ
+  public List<PlatformEntity> readPlatforms(final boolean isIncludeDeleted) {
+    log.debug("Read Platforms: [{}]", isIncludeDeleted);
+    return platformRepository.findAllPlatforms(isIncludeDeleted);
+  }
+
+  /** Use {@link CircularDependencyService#readPlatform(Long, boolean)} */
+  private PlatformEntity readPlatform(final Long id) {
+    log.debug("Read Platform: [{}]", id);
+    return platformRepository
+        .findById(id)
+        .orElseThrow(() -> new ElementNotFoundException("Platform", String.valueOf(id)));
+  }
+
+  // UPDATE
+  public PlatformEntity updatePlatform(final Long id, final PlatformRequest platformRequest) {
+    log.debug("Update Platform: [{}], [{}]", id, platformRequest);
+    final PlatformEntity platformEntity = readPlatform(id);
+
+    if (platformEntity.getDeletedDate() != null) {
+      throw new ElementNotActiveException("Platform", String.valueOf(id));
+    }
+
+    BeanUtils.copyProperties(platformRequest, platformEntity);
+    return platformRepository.save(platformEntity);
+  }
+
+  // DELETE
+  public PlatformEntity softDeletePlatform(final Long id) {
+    log.info("Soft Delete Platform: [{}]", id);
+    final PlatformEntity platformEntity = readPlatform(id);
+
+    if (platformEntity.getDeletedDate() != null) {
+      throw new ElementNotActiveException("Platform", String.valueOf(id));
+    }
+
+    platformEntity.setDeletedDate(LocalDateTime.now());
+    return platformRepository.save(platformEntity);
+  }
+
+  @Transactional
+  public void hardDeletePlatform(final Long id) {
+    log.info("Hard Delete Platform: [{}]", id);
+    final PlatformEntity platformEntity = readPlatform(id);
+
+    // before Platform can be deleted, we need to delete entities in PlatformProfileRole
+    platformProfileRoleService.hardDeletePlatformProfileRolesByPlatformIds(List.of(id));
+    // before Platform can be deleted, we need to delete entities in PlatformRolePermission
+    platformRolePermissionService.hardDeletePlatformRolePermissionsByPlatformIds(List.of(id));
+
+    platformRepository.delete(platformEntity);
+  }
+
+  // RESTORE
+  public PlatformEntity restoreSoftDeletedPlatform(final Long id) {
+    log.info("Restore Soft Deleted Platform: [{}]", id);
+    final PlatformEntity platformEntity = readPlatform(id);
+    platformEntity.setDeletedDate(null);
+    return platformRepository.save(platformEntity);
+  }
+}
