e138ba7fadcb9ddf7de3589eea5af2c0ff3e322e
Bibek Aryal <bibekaryal86@gmail.com>
2025-12-01 23:00:33 -0700

    SQL Tables

--- app/src/main/resources/db/migration/V2_1__task_service_permissions.sql
+++ app/src/main/resources/db/migration/V2_1__task_service_permissions.sql
@@ -1,124 +1,64 @@
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_TASK_CREATE', 'Can Add Task');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_TASK_READ', 'Can View Task(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_TASK_UPDATE', 'Can Update Task');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_TASK_DELETE', 'Can Delete Task');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_TASK_CREATE', 'Can Add Task');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_TASK_READ', 'Can View Task(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_TASK_UPDATE', 'Can Update Task');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_TASK_DELETE', 'Can Delete Task');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_TASK_CREATE', 'Can Add Task');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_TASK_READ', 'Can View Task(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_TASK_UPDATE', 'Can Update Task');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (4, 'TASKSVC_TASK_READ', 'Can View Task(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_CREATE', 'Can Add Task');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_READ', 'Can View Task(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_UPDATE', 'Can Update Task');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_SOFTDELETE', 'Can Soft Delete Task');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_HARDDELETE', 'Can Hard Delete Task');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_RESTORE', 'Can Restore Soft Deleted Task)');
 
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_TASK_INSTANCE_CREATE', 'Can Add Task Instance');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_TASK_INSTANCE_UPDATE', 'Can Update Task Instance');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_TASK_INSTANCE_DELETE', 'Can Delete Task Instance');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_TASK_INSTANCE_CREATE', 'Can Add Task Instance');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_TASK_INSTANCE_UPDATE', 'Can Update Task Instance');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_TASK_INSTANCE_DELETE', 'Can Delete Task Instance');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_TASK_INSTANCE_CREATE', 'Can Add Task Instance');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_TASK_INSTANCE_UPDATE', 'Can Update Task Instance');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (4, 'TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_INSTANCE_CREATE', 'Can Add Task Instance');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_INSTANCE_READ', 'Can View Task Instance(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_INSTANCE_UPDATE', 'Can Update Task Instance');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_INSTANCE_SOFTDELETE', 'Can Soft Delete Task Instance');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_INSTANCE_HARDDELETE', 'Can Hard Delete Task Instance');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_TASK_INSTANCE_RESTORE', 'Can Restore Soft Deleted Task Instance');
 
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_RECURRENCE_PATTERN_CREATE', 'Can Add Recurrence Pattern');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_RECURRENCE_PATTERN_READ', 'Can View Recurrence Pattern(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_RECURRENCE_PATTERN_UPDATE', 'Can Update Recurrence Pattern');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_RECURRENCE_PATTERN_DELETE', 'Can Delete Recurrence Pattern');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_RECURRENCE_PATTERN_CREATE', 'Can Add Recurrence Pattern');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_RECURRENCE_PATTERN_READ', 'Can View Recurrence Pattern(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_RECURRENCE_PATTERN_UPDATE', 'Can Update Recurrence Pattern');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_RECURRENCE_PATTERN_DELETE', 'Can Delete Recurrence Pattern');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_RECURRENCE_PATTERN_CREATE', 'Can Add Recurrence Pattern');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_RECURRENCE_PATTERN_READ', 'Can View Recurrence Pattern(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_RECURRENCE_PATTERN_UPDATE', 'Can Update Recurrence Pattern');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (4, 'TASKSVC_RECURRENCE_PATTERN_READ', 'Can View Recurrence Pattern(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_RP_CREATE', 'Can Add Recurrence Pattern');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_RP_READ', 'Can View Recurrence Pattern(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_RP_UPDATE', 'Can Update Recurrence Pattern');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_RP_SOFTDELETE', 'Can Soft Delete Recurrence Pattern');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_RP_HARDDELETE', 'Can Hard Delete Recurrence Pattern');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_RP_RESTORE', 'Can Restore Soft Deleted Recurrence Pattern');
 
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_NOTIFICATION_SETTING_CREATE', 'Can Add Notification Setting');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_NOTIFICATION_SETTING_READ', 'Can View Notification Setting(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_NOTIFICATION_SETTING_UPDATE', 'Can Update Notification Setting');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_NOTIFICATION_SETTING_DELETE', 'Can Delete Notification Setting');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_NOTIFICATION_SETTING_CREATE', 'Can Add Notification Setting');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_NOTIFICATION_SETTING_READ', 'Can View Notification Setting(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_NOTIFICATION_SETTING_UPDATE', 'Can Update Notification Setting');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_NOTIFICATION_SETTING_DELETE', 'Can Delete Notification Setting');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_NOTIFICATION_SETTING_CREATE', 'Can Add Notification Setting');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_NOTIFICATION_SETTING_READ', 'Can View Notification Setting(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_NOTIFICATION_SETTING_UPDATE', 'Can Update Notification Setting');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (4, 'TASKSVC_NOTIFICATION_SETTING_READ', 'Can View Notification Setting(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NS_CREATE', 'Can Add Notification Setting');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NS_READ', 'Can View Notification Setting(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NS_UPDATE', 'Can Update Notification Setting');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NS_SOFTDELETE', 'Can Soft Delete Notification Setting');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NS_HARDDELETE', 'Can Hard Delete Notification Setting');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NS_RESTORE', 'Can Restore Soft Deleted Notification Setting');
 
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_NOTIFICATION_CREATE', 'Can Add Notification');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_NOTIFICATION_UPDATE', 'Can Update Notification');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (1, 'TASKSVC_NOTIFICATION_DELETE', 'Can Delete Notification');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_NOTIFICATION_CREATE', 'Can Add Notification');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_NOTIFICATION_UPDATE', 'Can Update Notification');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (2, 'TASKSVC_NOTIFICATION_DELETE', 'Can Delete Notification');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_NOTIFICATION_CREATE', 'Can Add Notification');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (3, 'TASKSVC_NOTIFICATION_UPDATE', 'Can Update Notification');
-INSERT INTO permission (role_id, permission_name, permission_desc)
-VALUES (4, 'TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NOTIFICATION_CREATE', 'Can Add Notification');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NOTIFICATION_READ', 'Can View Notification(s)');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NOTIFICATION_UPDATE', 'Can Update Notification');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NOTIFICATION_SOFTDELETE', 'Can Soft Delete Notification');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NOTIFICATION_HARDDELETE', 'Can Hard Delete Notification');
+INSERT INTO permission (permission_name, permission_desc)
+VALUES ('TASKSVC_NOTIFICATION_RESTORE', 'Can Restore Soft Deleted Notification');
